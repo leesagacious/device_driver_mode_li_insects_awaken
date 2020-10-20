900bcd084755c0e15c0d18782b669e502f572736
leesagacious <sensor1010@163.com>
2020-10-20 04:11:26 -0700

    kernel/device: Declare header file functions such as device registration
    	device_add. device_remove. device_register. device_unregister
    
    Signed-off-by: leesagacious <sensor1010@163.com>

--- insects_awaken_device.h
+++ insects_awaken_device.h
@@ -5,3 +5,8 @@ struct base_device {
 	struct kobject base_kobj;
 	const char *base_device_name;
 };
+
+int base_device_add(struct base_device *);
+void base_device_remove(struct base_device *);
+int base_register(struct base_device *);
+void base_unregister(struct base_device *);
